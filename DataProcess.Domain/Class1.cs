a7d609f902e9dd86c9da6b1bfcb53a2a68f49919
Geraldo <geraldovilela19@gmail.com>
2022-04-25 19:54:05 -0400

    Add project files.

---
+++ DataProcess.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataProcess.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ WEBAPI.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WEBAPI.Domain
+{
+    public class Class1
+    {
+    }
+}
