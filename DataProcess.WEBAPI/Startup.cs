1d935cc1e72a82a0a6f0055c0f44b05578263169
Geraldo <geraldovilela19@gmail.com>
2022-04-25 22:44:33 -0400

    setting up docker-compose file;

--- DataProcess.WEBAPI/Startup.cs
+++ DataProcess.WEBAPI/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WEBAPI.Infra.Data;
 
 namespace DataProcess.WEBAPI
 {
@@ -26,6 +28,10 @@ namespace DataProcess.WEBAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = @"Server=db;Database=master;User=sa;Password=root123@;";
+            services.AddDbContextPool<AppDBContext>(
+                        options => options.UseSqlServer(connection));
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
